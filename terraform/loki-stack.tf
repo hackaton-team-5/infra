ea18844b1004a532081a644eb378325b934f56f9
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2022-02-05 14:35:41 +0100

    Push bad formated manifest

--- terraform/loki-stack.tf
+++ terraform/loki-stack.tf
@@ -26,3 +26,48 @@ resource "helm_release" "grafana" {
   values     = [file("../helm_values/grafana.yaml")]
 }
 
+resource "kubectl_manifest" "certificate_grafana" {
+  depends_on = [
+    helm_manifest.cluster_issuer
+  ]
+  yaml_body = <<YAML
+apiVersion: cert-manager.io/v1alpha2
+kind: Certificate
+metadata:
+  name: monitoring
+  namespace: hackathon-monitoring
+spec:
+  commonName: monitoring.hackathon-team5-cagip.site
+  secretName: monitoring-cert
+  dnsNames:
+    - monitoring.hackathon-team5-cagip.site
+  issuerRef:
+    name: letsencrypt
+    kind: ClusterIssuer
+YAML
+}
+
+resource "kubectl_manifest" "ingress_route_grafana" {
+  depends_on = [
+    helm_manifest.cluster_issuer
+  ]
+  yaml_body = <<YAML
+apiVersion: traefik.containo.us/v1alpha1
+kind: IngressRoute
+metadata:
+  name: monitoring
+  namespace: hackathon-monitoring
+spec:
+  entryPoints:
+    - websecure
+  routes:
+    - match: Host(`monitoring.hackathon-team5-cagip.site`)
+      kind: Rule
+      services:
+        - name: grafana
+          port: 80
+          namespace: hackathon-monitoring
+  tls:
+    secretName: monitoring-cert
+YAML
+}
